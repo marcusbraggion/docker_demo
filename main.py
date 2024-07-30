7581ba97be454604223a3df04711a0a79afb2e4f
Marcus Bragion <viniciusbragion@outlook.pt>
2024-07-30 18:22:22 -0300

    feat: adicionado main.py

---
+++ main.py
@@ -0,0 +1,10 @@
+import streamlit as st
+
+def main():
+    st.title(hello_world())
+
+def hello_world():
+    return "Minha primeira aplicação Web com Streamlit"
+
+if __name__ == '__main__':
+    main()
